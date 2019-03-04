0b15f06de389684d8fbd14ff6353bffa31d80c61
Amal Shehu <amalshehu@ashmxmz-titan.local>
2019-03-05 00:35:37 +0530

    Use mem

--- src/arrays.rs
+++ src/arrays.rs
@@ -1,7 +1,8 @@
+use std::mem;
 pub fn run() {
     // Array is a primitive type
     let arr: [i8; 8] = [1, 2, 3, 4, 5, 6, 7, 8]; // 1 bytes each
     println!("ARRAY: {:?}", arr);
     // Arrays are in stack
-    println!("size in bytes: {}", std::mem::size_of_val(&arr));
+    println!("size in bytes: {}", mem::size_of_val(&arr));
 }
