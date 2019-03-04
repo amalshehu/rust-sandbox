df606169e83b1583395113b1aec37cd9c3a168d4
Amal Shehu <amalshehu@ashmxmz-titan.local>
2019-03-04 22:37:32 +0530

    Add Basics + Positional print statements

--- src/print.rs
+++ src/print.rs
@@ -1,3 +1,6 @@
-pub fn run(){
-    println!("This is a test.")
+pub fn run() {
+    // Basics + Positional
+    println!("This is a test.");
+    println!("{}", 1000);
+    println!("Quick {} {}", "brown", "fox")
 }
