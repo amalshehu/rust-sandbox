ed511cea8cbf9c8f669333ea929fb3c536784b3a
Amal Shehu <amalshehu@gmail.com>
2019-03-07 23:42:38 +0530

    Add functions

---
+++ src/functions.rs
@@ -0,0 +1,7 @@
+pub fn run(number: i64) {
+    println!("SQUARE :{}", square(number))
+}
+
+fn square(num: i64) -> i64 {
+    num * num
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
-mod arrays;
-mod datatypes;
-mod loops;
-mod print;
-mod string;
-mod tuples;
-mod variables;
-mod vectors;
+// mod arrays;
+// mod datatypes;
+mod functions;
+// mod loops;
+// mod print;
+// mod string;
+// mod tuples;
+// mod variables;
+// mod vectors;
 fn main() {
     // print::run()
     // variables::run();
     // datatypes::run()
     // string::run()
     // vectors::run()
-    loops::run()
+    // loops::run()
+    functions::run(8);
 }
